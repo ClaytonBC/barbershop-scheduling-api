09475296d89feffde738bf195286702b1be4f5a8
Clayton Santos <claytonsantos20011@gmail.com>
2026-05-05 13:51:50 -0300

    chore: initial project setup with Spring Boot

---
+++ src/main/java/com/claytonbc/barbearia/BarbeariaAgendamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.claytonbc.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaAgendamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaAgendamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claytonbc/barbearia/BarbeariaAgendamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claytonbc.barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaAgendamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
